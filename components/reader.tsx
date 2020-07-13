1cf0eaedd6cd409a5e959749183af577bf564979
teraoka-k <teraoka.kennosuke@gmail.com>
2020-07-13 17:48:32 +0900

    refactor: Change name

--- components/reader.tsx
+++ components/reader.tsx
@@ -4,18 +4,15 @@ import 'swiper/components/pagination/pagination.scss'
 import 'swiper/components/scrollbar/scrollbar.scss'
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Note } from '../logic/colllections/note'
 import { NotePreview } from './notePreview'
+import { useNotes, notes } from '../states/notes'
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
-interface ReadModeProps {
-  notes: Note[]
-}
+export const Reader = () => {
+  useNotes()
 
-export const ReadMode = (props: ReadModeProps) => (
-  <div>
-    <button>write</button>
+  return (
     <Swiper
       spaceBetween={50}
       slidesPerView={3}
@@ -23,11 +20,11 @@ export const ReadMode = (props: ReadModeProps) => (
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
     >
-      {props.notes.map((note) => (
+      {notes.map((note) => (
         <SwiperSlide key={note._id}>
           <NotePreview note={note}></NotePreview>
         </SwiperSlide>
       ))}
     </Swiper>
-  </div>
-)
+  )
+}
