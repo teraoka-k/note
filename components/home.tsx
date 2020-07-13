787b95b1161fb6e5ab5ae32df7e4cd6d239191aa
teraoka-k <teraoka.kennosuke@gmail.com>
2020-07-13 17:47:19 +0900

    refactor: Create read button module

---
+++ components/home.tsx
@@ -0,0 +1,9 @@
+import { WriteButton } from './writeButton'
+import { ReadButton } from './readButton'
+
+export const Home = () => (
+  <div>
+    <WriteButton></WriteButton>
+    <ReadButton></ReadButton>
+  </div>
+)
