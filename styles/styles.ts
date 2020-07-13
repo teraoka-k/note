966e56dcf87957dbd3b3c6365b10ccf1fc5acaa2
teraoka-k <teraoka.kennosuke@gmail.com>
2020-07-13 19:52:33 +0900

    refactor: Remove unused style

--- styles/styles.ts
+++ styles/styles.ts
@@ -6,10 +6,3 @@ export const center: CSSProperties = {
   top: '50%',
   transform: 'translate(-50%, -50%)',
 }
-
-const extend = Object.assign(
-  {
-    backgroundColor: '#fff',
-  } as CSSProperties,
-  center
-)
