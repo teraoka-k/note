d6870679050f00ebedfadea6709951cb49f2808b
teraoka-k <teraoka.kennosuke@gmail.com>
2020-07-13 17:53:05 +0900

    refactor: Move note state to mogule

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,22 @@
-import { MdEditor } from '../components/mdEditor'
 import { center } from '../styles/styles'
-import { useState, useEffect } from 'react'
-import { playOpenSound } from '../util/sounds'
-import { Note } from '../logic/colllections/note'
 import {
   isHomeMode,
-  setReadMode,
   isWriteMode,
-  setHomeMode,
   isReadMode,
   useAppMode,
-} from '../logic/appMode'
-import { WriteButton } from '../components/writeButton'
-import { ReadMode } from '../components/readMode'
-import { ReadButton } from '../components/readButton'
+} from '../states/appMode'
+import { Reader } from '../components/reader'
+import { Writer } from '../components/writer'
+import { Home } from '../components/home'
+
 export default () => {
   useAppMode()
-  const [notes, setNotes] = useState([] as Note[])
-
-  useEffect(() => {
-    async function fetchNotes() {
-      const res = await fetch('api/notes')
-      const data = await res.json()
-      setNotes(data)
-    }
-    fetchNotes()
-  }, [])
 
   return (
     <div id="app" style={center}>
-      {isHomeMode() ? (
-        <div>
-          <WriteButton></WriteButton>
-          <ReadButton setNotes={setNotes}></ReadButton>
-        </div>
-      ) : null}
-      {isWriteMode() ? <MdEditor setHomeMode={setHomeMode}></MdEditor> : null}
-      {isReadMode() ? <ReadMode notes={notes}></ReadMode> : null}
+      {isHomeMode() ? <Home></Home> : null}
+      {isWriteMode() ? <Writer></Writer> : null}
+      {isReadMode() ? <Reader></Reader> : null}
     </div>
   )
 }
