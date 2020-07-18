46a28fb38b3fc9cd9434557f10ae42e482f6b51f
teraoka-k <teraoka.kennosuke@gmail.com>
2020-07-18 12:57:47 +0900

    refactor: Remove unused styles

--- components/notePreviewer.tsx
+++ components/notePreviewer.tsx
@@ -1,35 +1,35 @@
-import 'swiper/swiper.scss'
-import 'swiper/components/navigation/navigation.scss'
-import 'swiper/components/pagination/pagination.scss'
-import 'swiper/components/scrollbar/scrollbar.scss'
 import { NotePreview } from './notePreview'
 import { notes, useNotes } from '../states/notes'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Pagination, Scrollbar } from 'swiper'
 import { setViewingNote } from '../states/viewingNote'
 import { windowWidth } from '../states/windowSize'
-import { previewer } from '../styles/styles'
 
 SwiperCore.use([Pagination, Scrollbar])
 
 export const NotePreviewer = () => {
   useNotes()
   return (
-    <div style={previewer}>
-      <Swiper
-        spaceBetween={windowWidth / 100}
-        slidesPerView={3}
-        scrollbar={{ draggable: true }}
-        freeMode={true}
-      >
-        {notes.map((note) => (
-          <SwiperSlide key={note._id}>
-            <div onClick={() => setViewingNote(note)}>
-              <NotePreview note={note}></NotePreview>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <Swiper
+      slidesPerView={
+        windowWidth > 1000
+          ? 7
+          : windowWidth > 800
+          ? 5
+          : windowWidth > 300
+          ? 3
+          : 2
+      }
+      scrollbar={{ draggable: true }}
+      freeMode
+    >
+      {notes.map((note) => (
+        <SwiperSlide key={note._id}>
+          <div onClick={() => setViewingNote(note)}>
+            <NotePreview note={note}></NotePreview>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   )
 }
--- styles/styles.ts
+++ styles/styles.ts
@@ -1,59 +1,11 @@
 import { CSSProperties } from 'react'
 
-export const center: CSSProperties = {
-  position: 'absolute',
-  left: '50%',
-  top: '50%',
-  transform: 'translate(-50%, -50%)',
-}
 export const horizontallyCenter: CSSProperties = {
   display: 'table',
   margin: '0 auto',
 }
 
-/**
- *
- * @param from range (%)
- * @param to  range (%)
- */
-export const splitVertical = (from: number, to: number): CSSProperties => {
-  return {
-    top: `${from}%`,
-    height: `${to - from}%`,
-    width: '100%',
-    position: 'fixed',
-    left: 0,
-    right: 0,
-  }
-}
-
-/**
- *
- * @param from range (%)
- * @param to  range (%)
- */
-export const splitHorizontal = (from: number, to: number): CSSProperties => {
-  return {
-    left: `${from}%`,
-    width: `${to - from}%`,
-    height: '100%',
-    position: 'fixed',
-  }
-}
-
-export const middle = Object.assign(
-  splitVertical(46, 50),
-  splitHorizontal(45, 55)
-)
-
 export const buttons = Object.assign(
-  {
-    marginTop: '3%',
-  } as CSSProperties,
-  horizontallyCenter
-)
-
-export const previewer = Object.assign(
   {
     marginTop: '3%',
   } as CSSProperties,
@@ -63,15 +15,6 @@ export const previewer = Object.assign(
 export const viewer = Object.assign(
   {
     marginTop: '3%',
-  } as CSSProperties,
-  horizontallyCenter
-)
-
-export const editor = Object.assign(
-  {
-    width: '90%',
-    height: '45%',
-    overflow: 'auto',
   } as CSSProperties,
   horizontallyCenter
 )
